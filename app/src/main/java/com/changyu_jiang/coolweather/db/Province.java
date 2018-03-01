233a4d9909d08e489ddde9b4d841c40decd8a03a
changyujiang <changyu.jiang86@outlook.com>
2018-02-28 18:38:07 -0800

    config of litpal

---
+++ app/src/main/java/com/changyu_jiang/coolweather/db/Province.java
@@ -0,0 +1,37 @@
+package com.changyu_jiang.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Changyu on 2018/2/28.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinName) {
+        this.provinceName = provinName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
